04f3a4fae19289fe82313e65a73f0143c3d3dfd9
leopoldxueham <90619262+leopoldxueham@users.noreply.github.com>
2022-05-22 17:02:22 +0200

    Update fonctions.cpp

--- in104/repository/src/fonctions.cpp
+++ in104/repository/src/fonctions.cpp
@@ -2,7 +2,7 @@
 #include <SFML/Graphics/Sprite.hpp>
 #include "fonctions.h"
 
-
+// fonctions qui ajustent la taille des image avec la bonne proportion souhaitée. Up est le coefficient d'agrandissement à appliquer à la sprite Target
 void scaleToMinSize(sf::Sprite &target,int largeur,int hauteur){
     float sizex=target.getTexture()->getSize().x * target.getScale().x;
     float sizey=target.getTexture()->getSize().y * target.getScale().y;
